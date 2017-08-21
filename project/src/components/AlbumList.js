f57c8442d6fa3cd1bf4e694cbc7b8657d4beda12
abhishekvel <avelay@terpmail.umd.edu>
2017-08-20 20:37:10 -0400

    initial commit

---
+++ project/src/components/AlbumList.js
@@ -0,0 +1,13 @@
+// take both react library and component
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  return (
+    <View>
+      <Text> Album List!! </Text>
+    </View>
+  );
+};
+
+export default AlbumList;
